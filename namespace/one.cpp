1f518ee08ee4b50afc57ac40bb7115decd2049d7
Utshaw <farhan.tanvir.utshaw@gmail.com>
2018-12-30 14:20:03 +0600

    Update namespace

--- namespace/one.cpp
+++ namespace/one.cpp
@@ -1,10 +1,25 @@
+// https://www.geeksforgeeks.org/namespace-in-c/
 #include <iostream>
-// #include <bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
+#define endl '\n'
+
+int val = 100;
+
+namespace first {
+    int val = 200;
+}
+
 int main() {
     
-    printf("This is some\n");
+    int val = 300;
+
+    cout << "Local variable: " << val << endl;
+    cout << "Namespace variable: " << first::val << endl;
+    cout << "Global variable: " << ::val << endl; // :: is a Unary Scope resolution operator
+
+
     return 0;
-} 
+}
